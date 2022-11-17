89f1672e59a2d708f55f0ba634908686b9417d23
hyunnano <nepocean99@gmail.com>
2022-11-17 17:08:16 +0900

    commit

--- src/main/java/net/ict/ict01/Controller/SampleController.java
+++ src/main/java/net/ict/ict01/Controller/SampleController.java
@@ -7,11 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
 public class SampleController {
+
+    class SampleDTO{
+        private String p1,p2,p3;
+        public String getP1(){return p1;}
+        public String getP2(){return p2;}
+        public String getP3(){return p3;}
+
+    }
 
     @GetMapping("/hello")
     public void hello(Model model){
@@ -23,6 +35,33 @@ public class SampleController {
     public void ex1(Model model){
         List<String> list = Arrays.asList("AAA","BBB","CCC");
         model.addAttribute("list",list);
+    }
+
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model){
+        log.info("ex/ex2!!!!!!!!!!!!!!!!!!!!!");
+        List<String> strList = IntStream.range(1,11) // strList에 1부터10까지 넣음
+                .mapToObj(i->"Data"+i) // 맵형태로 객체 생성
+                .collect(Collectors.toList());
+        model.addAttribute("list",strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A","aaa");
+        map.put("B","bbb");
+        map.put("C","ccc");
+        model.addAttribute("map",map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "value -- p1!";
+        sampleDTO.p2 = "value -- p2!";
+        sampleDTO.p3 = "value -- p3!";
+        model.addAttribute("dto",sampleDTO);
+    }
+
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model){
+        model.addAttribute("arr",new String[]{"kkk","jjj","mmm"});
 
     }
 
